6249b054d55ac1efe5f1c8f7ebb52b49ca6d3b71
AndreSouto <andre_luisferreira@hotmail.com>
2018-01-29 23:55:37 -0200

    Faltando inserir codigo binario de processamento de imagem no aplicativo

--- PibicApplication/app/src/main/java/com/example/andre/pibicapplication/ProcessingActivity.java
+++ PibicApplication/app/src/main/java/com/example/andre/pibicapplication/ProcessingActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class ProcessingActivity extends AppCompatActivity {
 
     ImageView pic;
@@ -20,5 +24,19 @@ public class ProcessingActivity extends AppCompatActivity {
         String path = "sdcard/camera_app/cam_image.jpg";
         pic.setImageDrawable(Drawable.createFromPath(path));
 
+        try {
+            callExe();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void callExe() throws IOException {
+
+        //String myExec = "/data/data/APPNAME/FILENAME";
+        //Process process = Runtime.getRuntime().exec(myExec);
+        //DataOutputStream os = new DataOutputStream(process.getOutputStream());
+        //DataInputStream osRes = new DataInputStream(process.getInputStream());
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Aplicativo PIBIC 2017/2018 - Arcus Senilis
 
 Andre Luis, Victor Araujo Bonitao Tcheco
+
+PRÓXIMO PASSO:
+
+INSERIR BINÁRIO NO APLICATIVO: LER FONTE http://gimite.net/en/index.php?Run%20native%20executable%20in%20Android%20App
