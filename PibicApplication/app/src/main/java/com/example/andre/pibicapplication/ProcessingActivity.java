253d9161011720a4968e529084d38fd06bd9e019
AndreSouto <andre_luisferreira@hotmail.com>
2018-05-21 17:24:20 +0200

    funcionando

--- PibicApplication/app/src/main/java/com/example/andre/pibicapplication/ProcessingActivity.java
+++ PibicApplication/app/src/main/java/com/example/andre/pibicapplication/ProcessingActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,6 +34,7 @@ public class ProcessingActivity extends AppCompatActivity {
 
     String path = "sdcard/camera_app/cam_image.jpg";
     ImageView pic;
+    TextView tx3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,8 @@ public class ProcessingActivity extends AppCompatActivity {
          /* ImageView onde foto tirada aparece */
         pic = (ImageView) findViewById(R.id.imageView2);
         pic.setImageDrawable(Drawable.createFromPath(path));
+
+        tx3 = (TextView) findViewById(R.id.textView3);
 
         try {
             sendToServer();
@@ -84,6 +89,7 @@ public class ProcessingActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
 
             String data = "";
+            StringBuffer buffer = new StringBuffer();
 
             HttpURLConnection httpURLConnection = null;
             try {
@@ -100,9 +106,21 @@ public class ProcessingActivity extends AppCompatActivity {
                 wr.flush();
                 wr.close();
 
+
                 if (httpURLConnection.getResponseCode() != 200) {
                     throw new RuntimeException("Failed : HTTP error code : "
                             + httpURLConnection.getErrorStream());
+                }
+
+
+                InputStream stream = httpURLConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+
+
+                while((data = reader.readLine()) != null)
+                {
+                    buffer.append(data);
                 }
 
 
@@ -114,13 +132,31 @@ public class ProcessingActivity extends AppCompatActivity {
                 }
             }
 
-            return data;
+            return String.valueOf(buffer);
         }
 
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            Log.e("TAG", result);
+
+            JSONObject mainObject = null;
+            String name = "";
+
+            try {
+                mainObject = new JSONObject(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                 name = mainObject.getString("resposta");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            tx3.setText(name);
+            Toast.makeText(getApplicationContext(), name,
+                    Toast.LENGTH_LONG).show();
         }
     }
 }
